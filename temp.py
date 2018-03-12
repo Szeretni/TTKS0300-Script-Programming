1551fca877d0e2b9d5c4161cda1fe9333f8e382e
Hannu Oksman <hannu@muaddibs.net>
2018-03-12 10:21:20 +0200

    Assignment 30 done

--- temp.py
+++ temp.py
@@ -1,41 +1,4 @@
-﻿#Version 2018-02-19
-'''
-Extend the function in the previous assignment to handle TypeError which is caused when you try to
-divide a number with a non-number (e.g. character).
-'''
-def calculator(oper,num1,num2):
-    try:
-        if (oper == "add"):
-            result = num1 + num2
-        elif (oper == "sub"):
-            result = num1 - num2
-        elif (oper == "multiply"):
-            result = num1 * num2
-        elif (oper == "divide"):
-            result = num1 / num2
-    except TypeError:
-            result = "TypeError occurred."
-    except ZeroDivisionError:
-            result =  None
-    print result
+f = lambda x,y: x+1
+nums = [1,2,3,4,5]
 
-#calls
-calculator("add", 1, 2)
-calculator("sub", 1, 2)
-calculator("multiply", 1, 2)
-calculator("divide",5,2.0)
-calculator("divide",5,0)
-calculator("divide",5,'a')
-calculator("divide",5,"a")
-#calculator("divide",5,5⁄2) Doesn't work. Can't send operations as args?
-
-#prints
-'''
-3
--1
-2
-2.5
-None
-TypeError occurred.
-TypeError occurred.
-'''
+print reduce(f, nums)
