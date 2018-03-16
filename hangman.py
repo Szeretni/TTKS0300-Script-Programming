b68db867d83c24b278431c87e5151ec074b66639
Hannu Oksman <hannu@muaddibs.net>
2018-03-16 15:55:03 +0200

    Hangman done

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-﻿#Hannu Oksman 2018-03-12
+﻿#Hannu Oksman 2018-03-16
 #Hangman
 '''
 Write a hangman game in Python. The game should
@@ -54,62 +54,162 @@ import random
 import sys
 
 def hangman():
-    #words from the file and number of words in the file
-    wordList = []
-    words = -1
-    f = open("hangman_words.txt","r")
-    for line in f:
-        wordList.append(line)
-    words += 1
-    f.close()
+#initialization begins
+    #generating list of words and counting how many words are available
+    wordList = ReadFile()
+    wordsInList = len(wordList)
 
     #random word from the file
-    rand = random.randint(0,words)
-    theWord = wordList[rand]
+    theWord = RandomWord(wordList,wordsInList)
 
     #the game
     #topic
     print "Hangman game!\nThe word has " + str(len(theWord)) + " letters"
 
     #number of guesses
-    maxGuesses = 7
+    maxGuesses = 10 #increased to 10 from specs 7 because of words like "underscore"
 
-    #visible word
-    visibleWord = ""
-    for x in range(0,len(theWord)):
-        visibleWord += "_"
-
-    #validation
     #valid chars
-    validChars = []
-    numOfLetters = ord('z')-ord('a') #ascii arithmetics, syntax from https://stackoverflow.com/questions/2156892/python-how-can-i-increment-a-char
-    for x in range(0,numOfLetters+1):
-        validChars.append(chr(ord('a')+x))
+    validChars = ValidChars()
 
-    #lenght, valid char
+    #init visible word
+    visibleWord = VisibleWord(theWord)
+
+    #win flag
+    win = False
+
+#initialization done
+
+#guessing begins
+    #guess-loop
     while (maxGuesses > 0):
+        #asking for a guess
         guess = raw_input("Guess: ")
-        if len(guess) > 1:
-            print "Too long guess, try again."
-        elif not validGuess(guess,validChars):
-            print "Not a valid character, try again."
-        else:
-            #valid guess, checks if guess char is in the word
-            counter = 1
-            for c in theWord:
-                if guess is c:
-                    visibleWord[counter]=c
-            print visibleWord
+        #is the guess valid?
+        validation = Validation(guess,validChars)
+        if (validation):
+            #valid guess
+            #updating visible word
+            visibleWord = UpdateVisibleWord(visibleWord,theWord,guess)
+            #print visible word
+            PrintVisible(visibleWord) #visible word is a list, so print isn't pretty without this
+            #wincheck
+            win = WinCheck(visibleWord)
+            if win:
+                break
+            #updating the number of available guesses
             maxGuesses -= 1
+        
+    #out of the guess-loop, game over
+    if win:
+        print "You guessed the word!"
+    else:
+        print "Game over, you didn't guess the word!"
+#guessing done
 
-#functions    
+
+
+
+
+#functions
+#words from the file
+def ReadFile():
+    wordList = []
+    f = open("hangman_words.txt","r")
+    for line in f:
+        wordList.append(line.strip()) #in order to remove newline \n https://stackoverflow.com/questions/4319236/remove-the-newline-character-in-a-list-read-from-a-file
+    f.close()
+    return wordList
+
+#the word from the list
+def RandomWord(wordList,wordsInList):
+    rand = random.randint(0,wordsInList-1) #-1 to avoid out of index
+    theWord = wordList[rand]
+    return theWord
+
+#valid chars
+def ValidChars():
+    validChars = []
+    numOfLetters = ord('z')-ord('a') #ascii arithmetics, python syntax from https://stackoverflow.com/questions/2156892/python-how-can-i-increment-a-char
+    for x in range(0,numOfLetters+1): #+1 to include last int
+        validChars.append(chr(ord('a')+x))
+    return validChars
+
+#visible word init
+def VisibleWord(theWord):
+    visibleWord = []
+    for x in range(0,len(theWord)):
+        visibleWord.append('_')
+    return visibleWord
+
+#validation
+def Validation(guess,validChars):
+    isValid = False
+    #is lenght valid?
+    if len(guess) > 1:
+        print "Too long guess, try again."
+    #is char valid?
+    elif not ValidGuess(guess,validChars):
+        print "Not a valid character, try again."
+    else:
+        isValid = True
+    return isValid
+
 #valid guess
-def validGuess(guess,validChars):
+def ValidGuess(guess,validChars):
     valid = False
     for c in validChars:
         if guess is c:
+            #guessed char is a valid char
             valid = True
     return valid
 
+#update visible word
+def UpdateVisibleWord(visibleWord,theWord,guess):
+    index=0
+    for c in theWord:
+        #if char is in the word, update visible word
+        if guess is c:
+            visibleWord[index]=c
+        index += 1
+    return visibleWord
+
+#print visible word
+def PrintVisible(visibleWord):
+    for x in visibleWord:
+        sys.stdout.write(x) #no newline
+    print
+
+#wincheck
+def WinCheck(visibleWord):
+    win = True
+    for c in visibleWord:
+        if c == '_':
+            #underscore-char found, therefore player hasn't guessed all of the chars
+            win = False
+    return win
+
+
+
+
+
 #init
 hangman()
+
+'''
+ RESTART: C:\Users\l2912\Source\Repos\TTKS0300-Script-Programming\hangman.py 
+Hangman game!
+The word has 7 letters
+Guess: h
+h______
+Guess: a
+ha___a_
+Guess: n
+han__an
+Guess: g
+hang_an
+Guess: m
+hangman
+You guessed the word!
+>>> 
+'''
